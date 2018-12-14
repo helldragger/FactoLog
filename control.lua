0c78bc3bf38f09ce79e2df059641137761f41485
Helldragger <christopher1223334444@live.fr>
2018-12-15 00:30:46 +0100

    Added 4 data types, fixed log name change

--- control.lua
+++ control.lua
@@ -3,9 +3,88 @@
 require 'stdlib/table'
 require 'stdlib/log/logger'
 require 'serpent'
+running = false
+--ITEM CACHE MECHANICS
+ItemNameCache = {}
 
-Loggers = {}
+function getLocalisedName(name)
+   if ItemNameCache[name] == nil then
+      if not game.item_prototypes[name] == nil then
+         ItemNameCache[name] = game.item_prototypes[name].localised_name
+      elseif not game.fluid_prototypes[name] == nil then   
+         ItemNameCache[name] = game.fluid_prototypes[name].localised_name 
+      elseif not game.entity_prototypes[name] == nil then   
+         ItemNameCache[name] = game.entity_prototypes[name].localised_name 
+      else
+         ItemNameCache[name] = name
+      end
+   end
+   return ItemNameCache[name]
+end
 
+
+
+--CONFIG MECHANICS
+Config = {}
+ConfigName = {}
+ConfigSettingsList = {}
+LastLogfileName = "default"
+
+function addConfigSetting(alias, setting_name)
+   Config[setting_name] = nil
+   ConfigName[alias] = setting_name
+   ConfigSettingsList[#ConfigSettingsList + 1] = setting_name
+end
+
+function loadConfigSetting(setting_name)
+   local value = settings.global[setting_name].value
+   if ConfigName["StatsTimeDelta"] == setting_name then
+      local StatsTimeDelta = 60*60
+      if (value == "5s") then
+         StatsTimeDelta = 5*60
+      elseif (value == "10s") then
+         StatsTimeDelta = 10*60
+      elseif (value == "30s") then
+         StatsTimeDelta = 30*60
+      elseif (value == "1m") then
+         StatsTimeDelta = 60*60
+      elseif (value == "5m") then
+         StatsTimeDelta = 300*60
+      elseif (value == "10m") then
+         StatsTimeDelta = 600*60
+      elseif (value == "30m") then
+         StatsTimeDelta = 1800*60
+      elseif (value == "1h") then
+         StatsTimeDelta = 3600*60
+      end
+      value = StatsTimeDelta
+   elseif ConfigName["LogfileName"] == setting_name and running then
+      --move the last logfile if it was the default one (temporary folder)
+      LastLogfileName = value
+      table.each(game.forces, modifyLoggers)
+   end
+   Config[setting_name]=value
+end
+
+function getConfigSetting(alias)
+   return Config[ConfigName[alias]]
+end
+
+addConfigSetting("SolidProductionMode","FactoLog-2-solid-production-statistics")
+addConfigSetting("FluidProductionMode","FactoLog-2-fluid-production-statistics")
+addConfigSetting("KillCountMode","FactoLog-2-kill-count-statistics")
+addConfigSetting("EntityBuildMode","FactoLog-2-entity-build-statistics")
+addConfigSetting("LogfileName","FactoLog-1-logfile-name")
+addConfigSetting("isLogging","FactoLog-3-logging-activated")
+addConfigSetting("StatsTimeDelta","FactoLog-1-statistics-logging-interval")
+addConfigSetting("ItemsLaunchedMode", "FactoLog-2-launched-items-statistics")
+addConfigSetting("RocketLaunchedMode", "FactoLog-2-launched-rockets-statistics")
+addConfigSetting("EvolutionFactorMode", "FactoLog-2-evolution-factor-statistics")
+
+<<<<<<< HEAD
+for alias,realsettingname in pairs(ConfigName) do
+   loadConfigSetting(realsettingname)
+=======
 SolidProductionMode = settings.global["solid-production-statistics"].value
 FluidProductionMode = settings.global["fluid-production-statistics"].value
 KillCountMode = settings.global["kill-count-statistics"].value
@@ -37,6 +116,7 @@ elseif (interval == "30m") then
    StatsTimeDelta = 1800*60
 elseif (interval == "1h") then
    StatsTimeDelta = 3600*60
+>>>>>>> b2588156e48aa5119c51bd76aa9b629539ca484d
 end
 
 function reloadConfig()
@@ -63,17 +143,23 @@ function reloadConfig()
 end
 
 
+-- LOGGING MECHANICS
+Loggers = {}
 function getLogger(name)
    if (Loggers[name] == nil) then
-      Loggers[name] = Logger.new('FactoLog_'..FolderName, "history", true, {force_append=true, log_ticks=true})
+      Loggers[name] = Logger.new('FactoLog', getConfigSetting("LogfileName"), true, {force_append=true, log_ticks=true})
    end
    return Loggers[name]
+end
+
+function modifyLoggers(force)
+   Loggers[force.name] = Logger.new('FactoLog', getConfigSetting("LogfileName"), true, {force_append=true, log_ticks=true})
 end
 
 function prodDataToStr(data)
    local t = { }
    for k,v in pairs(data) do
-      t[#t+1] = tostring(k)
+      t[#t+1] = getLocalisedName(tostring(k))
       t[#t+1] = ":"
       t[#t+1] = tostring(v)
       t[#t+1] = ";"
@@ -82,13 +168,21 @@ function prodDataToStr(data)
 end
 
 function arrayStatsToStr(name, data)
+<<<<<<< HEAD
+   local str = {name, ";INPUT;"}
+=======
    local str = {name, ";"}
+>>>>>>> b2588156e48aa5119c51bd76aa9b629539ca484d
    str[#str+1] = prodDataToStr(data)
    return table.concat(str, "")
 end
 
 function simpleStatsToStr(name, data)
+<<<<<<< HEAD
+   return table.concat({name, ";INPUT;", getLocalisedName(tostring(data)), ";"}, "")
+=======
    return table.concat({name, ";", tostring(data), ";"}, "")
+>>>>>>> b2588156e48aa5119c51bd76aa9b629539ca484d
 end
 
 function prodStatsToStr(statName, stats, MODE)
@@ -112,18 +206,34 @@ end
 function dumpForceStats(force)
    --productions and other flow statistics of the game
    local stats = {";STATS;"..force.name..";"}
+<<<<<<< HEAD
+   if (not (getConfigSetting("SolidProductionMode") == "none")) then
+      stats[#stats + 1] = prodStatsToStr("SOLID", force.item_production_statistics, getConfigSetting("SolidProductionMode"))
+   end
+   if (not (getConfigSetting("FluidProductionMode") == "none")) then
+      stats[#stats + 1] = prodStatsToStr("FLUID", force.fluid_production_statistics, getConfigSetting("FluidProductionMode"))
+=======
    if (not (SolidProductionMode == "none")) then
       stats[#stats + 1] = prodStatsToStr("SOLID", force.item_production_statistics, SolidProductionMode)
+>>>>>>> b2588156e48aa5119c51bd76aa9b629539ca484d
    end
-   if (not (FluidProductionMode == "none")) then
-      stats[#stats + 1] = prodStatsToStr("FLUID", force.fluid_production_statistics, FluidProductionMode)
+   if (not (getConfigSetting("KillCountMode") == "none")) then
+      stats[#stats + 1] = prodStatsToStr("KILLS", force.kill_count_statistics, getConfigSetting("KillCountMode"))
    end
-   if (not (KillCountMode == "none")) then
-      stats[#stats + 1] = prodStatsToStr("KILLS", force.kill_count_statistics, KillCountMode)
+   if (not (getConfigSetting("EntityBuildMode") == "none")) then
+      stats[#stats + 1] = prodStatsToStr("BUILD", force.entity_build_count_statistics, getConfigSetting("EntityBuildMode"))
    end
-   if (not (EntityBuildMode == "none")) then
-      stats[#stats + 1] = prodStatsToStr("BUILD", force.entity_build_count_statistics, EntityBuildMode)
+   if (getConfigSetting("ItemsLaunchedMode")) then
+      stats[#stats + 1] = arrayStatsToStr("LAUNCHED", force.items_launched)
    end
+<<<<<<< HEAD
+   if (getConfigSetting("RocketLaunchedMode")) then
+      stats[#stats + 1] = simpleStatsToStr("ROCKETS", force.rockets_launched)
+   end
+   if (getConfigSetting("EvolutionFactorMode")) then
+      stats[#stats + 1] = simpleStatsToStr("EVOLUTION", force.evolution_factor)
+   end
+=======
    if (ItemsLaunchedMode) then
       stats[#stats + 1] = arrayStatsToStr("LAUNCHED", force.items_launched)
    end
@@ -139,6 +249,7 @@ function dumpForceStats(force)
    if (TechnologyReminderMode) then
       --stats[#stats + 1] = techStatsToStr("AI", force.technologies)
    end
+>>>>>>> b2588156e48aa5119c51bd76aa9b629539ca484d
 
    getLogger(force.name).log(table.concat(stats,""))
 end
@@ -146,10 +257,10 @@ end
 
 script.on_event({defines.events.on_tick},
    function (e)
-      if e.tick % StatsTimeDelta == 0 then --common trick to reduce how often this runs, we don't want it running every tick, just 1/second
-         print("data time!")
-         table.each(game.forces, dumpForceStats)
-
+      if getConfigSetting("isLogging") then
+         if e.tick % getConfigSetting("StatsTimeDelta") == 0 then --common trick to reduce how often this runs, we don't want it running every tick, just 1/second
+            table.each(game.forces, dumpForceStats)
+         end
       end
    end
 )
@@ -168,10 +279,23 @@ script.on_event({defines.events.on_forces_merged},
 
 script.on_event({defines.events.on_runtime_mod_setting_changed},
    function (e)
+<<<<<<< HEAD
+      for _,v in pairs(ConfigSettingsList) do
+         if e.setting == v then
+            loadConfigSetting(e.setting)
+            break
+         end
+      end
+      
+   end
+)
+=======
       if(e.setting == "statistics-logging-interval")then
          reloadConfig()
       end
    end
 )
 
+>>>>>>> b2588156e48aa5119c51bd76aa9b629539ca484d
 
+running = true
--- settings.lua
+++ settings.lua
@@ -1,7 +1,7 @@
 data:extend({
     {
         type = "string-setting",
-        name = "solid-production-statistics",
+        name = "FactoLog-2-solid-production-statistics",
         setting_type = "runtime-global",
         default_value = "none",
         allowed_values = {"none", "both", "input", "output"}
@@ -11,7 +11,7 @@ data:extend({
 data:extend({
     {
         type = "string-setting",
-        name = "fluid-production-statistics",
+        name = "FactoLog-2-fluid-production-statistics",
         setting_type = "runtime-global",
         default_value = "none",
         allowed_values = {"none", "both", "input", "output"}
@@ -21,7 +21,7 @@ data:extend({
 data:extend({
     {
         type = "string-setting",
-        name = "kill-count-statistics",
+        name = "FactoLog-2-kill-count-statistics",
         setting_type = "runtime-global",
         default_value = "none",
         allowed_values = {"none", "both", "input", "output"}
@@ -31,7 +31,7 @@ data:extend({
 data:extend({
     {
         type = "string-setting",
-        name = "entity-build-statistics",
+        name = "FactoLog-2-entity-build-statistics",
         setting_type = "runtime-global",
         default_value = "none",
         allowed_values = {"none", "both", "input", "output"}
@@ -41,7 +41,7 @@ data:extend({
 data:extend({
     {
         type = "string-setting",
-        name = "statistics-logging-interval",
+        name = "FactoLog-1-statistics-logging-interval",
         setting_type = "runtime-global",
         default_value = "1m",
         allowed_values = {"5s", "10s", "30s", "1m", "5m", "10m", "30m", "1h", "5h"}
@@ -51,8 +51,45 @@ data:extend({
 data:extend({
     {
         type = "string-setting",
-        name = "logfile-folder-name",
+        name = "FactoLog-1-logfile-name",
         setting_type = "runtime-global",
         default_value = "default"
     }
 })
+
+data:extend({
+    {
+        type = "bool-setting",
+        name = "FactoLog-3-logging-activated",
+        setting_type = "runtime-global",
+        default_value = false
+    }
+})
+
+data:extend({
+    {
+        type = "bool-setting",
+        name = "FactoLog-2-launched-items-statistics",
+        setting_type = "runtime-global",
+        default_value = false
+    }
+})
+
+data:extend({
+    {
+        type = "bool-setting",
+        name = "FactoLog-2-launched-rockets-statistics",
+        setting_type = "runtime-global",
+        default_value = false
+    }
+})
+
+data:extend({
+    {
+        type = "bool-setting",
+        name = "FactoLog-2-evolution-factor-statistics",
+        setting_type = "runtime-global",
+        default_value = false
+    }
+})
+
