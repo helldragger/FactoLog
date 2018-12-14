b2588156e48aa5119c51bd76aa9b629539ca484d
Helldragger <christopher1223334444@live.fr>
2018-12-14 05:06:53 +0100

    Modified the data format to 'timestamp :;datatype;datainfo;data' and prepared some new potential features

--- control.lua
+++ control.lua
@@ -11,8 +11,16 @@ FluidProductionMode = settings.global["fluid-production-statistics"].value
 KillCountMode = settings.global["kill-count-statistics"].value
 EntityBuildMode = settings.global["entity-build-statistics"].value
 FolderName = settings.global["logfile-folder-name"].value
+--todo add those to the settings
+ItemsLaunchedMode = false
+RocketLaunchedMode = false
+EvolutionFactorMode = false
+AIReminderMode = false
+TechnologyReminderMode = false
+
 local interval = settings.global["statistics-logging-interval"].value
 StatsTimeDelta = 60*60
+
 if (interval == "5s") then
    StatsTimeDelta = 5*60
 elseif (interval == "10s") then
@@ -29,6 +37,29 @@ elseif (interval == "30m") then
    StatsTimeDelta = 1800*60
 elseif (interval == "1h") then
    StatsTimeDelta = 3600*60
+end
+
+function reloadConfig()
+   local interval = settings.global["statistics-logging-interval"].value
+   StatsTimeDelta = 60*60
+
+   if (interval == "5s") then
+      StatsTimeDelta = 5*60
+   elseif (interval == "10s") then
+      StatsTimeDelta = 10*60
+   elseif (interval == "30s") then
+      StatsTimeDelta = 30*60
+   elseif (interval == "1m") then
+      StatsTimeDelta = 60*60
+   elseif (interval == "5m") then
+      StatsTimeDelta = 300*60
+   elseif (interval == "10m") then
+      StatsTimeDelta = 600*60
+   elseif (interval == "30m") then
+      StatsTimeDelta = 1800*60
+   elseif (interval == "1h") then
+      StatsTimeDelta = 3600*60
+   end
 end
 
 
@@ -50,6 +81,16 @@ function prodDataToStr(data)
    return table.concat(t,"")
 end
 
+function arrayStatsToStr(name, data)
+   local str = {name, ";"}
+   str[#str+1] = prodDataToStr(data)
+   return table.concat(str, "")
+end
+
+function simpleStatsToStr(name, data)
+   return table.concat({name, ";", tostring(data), ";"}, "")
+end
+
 function prodStatsToStr(statName, stats, MODE)
    ---solid
    local statsArray = {statName .. ";"}
@@ -58,21 +99,19 @@ function prodStatsToStr(statName, stats, MODE)
       statsArray[#statsArray+1] ="INPUT;" 
       --statsArray[#statsArray+1] =serpent.line(stats.input_counts)
       statsArray[#statsArray+1] =prodDataToStr(stats.input_counts)
-      statsArray[#statsArray+1] =";"
    end
    if (MODE == "both" or MODE == "output") then
       --output
       statsArray[#statsArray+1] ="OUTPUT;"
       --statsArray[#statsArray+1] =serpent.line(stats.output_counts)
       statsArray[#statsArray+1] =prodDataToStr(stats.output_counts)
-      statsArray[#statsArray+1] =";"
    end
    return table.concat(statsArray, "")
 end
 
 function dumpForceStats(force)
    --productions and other flow statistics of the game
-   local stats = {force.name..";"}
+   local stats = {";STATS;"..force.name..";"}
    if (not (SolidProductionMode == "none")) then
       stats[#stats + 1] = prodStatsToStr("SOLID", force.item_production_statistics, SolidProductionMode)
    end
@@ -85,6 +124,22 @@ function dumpForceStats(force)
    if (not (EntityBuildMode == "none")) then
       stats[#stats + 1] = prodStatsToStr("BUILD", force.entity_build_count_statistics, EntityBuildMode)
    end
+   if (ItemsLaunchedMode) then
+      stats[#stats + 1] = arrayStatsToStr("LAUNCHED", force.items_launched)
+   end
+   if (RocketLaunchedMode) then
+      stats[#stats + 1] = simpleStatsToStr("ROCKETS", force.rockets_launched)
+   end
+   if (EvolutionFactorMode) then
+      stats[#stats + 1] = simpleStatsToStr("EVOLUTION", force.evolution_factor)
+   end
+   if (AIReminderMode) then
+      stats[#stats + 1] = simpleStatsToStr("AI", force.ai_controllable)
+   end
+   if (TechnologyReminderMode) then
+      --stats[#stats + 1] = techStatsToStr("AI", force.technologies)
+   end
+
    getLogger(force.name).log(table.concat(stats,""))
 end
 
@@ -101,13 +156,21 @@ script.on_event({defines.events.on_tick},
 
 script.on_event({defines.events.on_research_finished},
    function (e)
-      getLogger(e.research.force.name).log("RESEARCHED;"..e.research.name)
+      getLogger(e.research.force.name).log(";EVENT;"..e.research.force.name..";RESEARCHED;"..e.research.name..";")
    end
 )
 
 script.on_event({defines.events.on_forces_merged},
    function (e)
       Loggers[e.source_name] = nil
+   end
+)
+
+script.on_event({defines.events.on_runtime_mod_setting_changed},
+   function (e)
+      if(e.setting == "statistics-logging-interval")then
+         reloadConfig()
+      end
    end
 )
 
